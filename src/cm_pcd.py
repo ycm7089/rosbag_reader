513fffbdd62fc1c76c57f8d80298e20def76dba7
ycm7089 <ycm7089@naver.com>
2022-10-13 00:42:29 +0900

    menboong

--- src/cm_pcd.py
+++ src/cm_pcd.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+from hashlib import new
+from traceback import print_tb
 import rospy
 import sys
 import rosbag
 import pcl
-# import pcl_helper
 import numpy as np
 
 def example():
@@ -14,29 +15,58 @@ def example():
     pc = pcl.PointCloud(pc_array)
     print(pc)
     # output is <PointCloud of 2 points>
+def eee():
+    arr = np.empty((0,3), int)
+    arr = np.append(arr, np.array([[1, 2, 3]]), axis=0)
+    for i in range(1,10) :
+        arr = np.append(arr, np.array([[i, i, i]]), axis=0)
+    print(arr)
+    
 
 def read_bagfile():
     bag = rosbag.Bag("/home/cm/livox_data.bag")
     count = 0
+    new_array = np.empty((0,3))
     for topic, msg, t in bag.read_messages(topics=['/livox/lidar']):
-        print(type(msg.points[count].x))
-        pc_array = np.array([round(msg.points[count].x, 3), round(msg.points[count].y,3),
-                             round(msg.points[count].z,3)], dtype=np.float64)
-        print(type(pc_array))
-        pc = pcl.PointCloud()
-        print(type(pc))
-        print(pc_array)
-        print(pc)
-        
-        # pc.from_array(pc_array)
-        # pc = pcl_helper.XYZ_to_XYZRGB(pc,[255,255,255])
+        # print(type(msg.points[count].x))
+        # if count >= 1:
+            # pc_array = np.array([[msg.points[count-1].x,msg.points[count-1].y, msg.points[count-1].z],
+                                # [msg.points[count].x,msg.points[count].y, msg.points[count].z]], dtype=np.float32)
+        if count == 0 :
+            pc_array = np.array([[msg.points[count].x,msg.points[count].y, msg.points[count].z]], dtype=np.float32)
+        else :
+            pc_array = np.append(pc_array, np.array([[msg.points[count].x, msg.points[count].y, msg.points[count].z]]), axis=0)
+            
+            
+        # if count <3 :
+        #     print(pc_array)
+        # if count >1 :
+           
+        # print(pc_array)
         # print(pc)
+        # pcl.save(pc,"/home/kimm/pcd_data/"+ str(count) + ".pcd")
+        
         count = count + 1
         # print(len(msg))
-        
+
+    for i in range(0,int(count/2)) :
+        # 0 ~ 2335  count is 2336
+        if i >=1 :      
+            new_array = np.array([pc_array[2 * i-1], pc_array[2 * i]], dtype=np.float32)
+            print(new_array)
+        elif i == int(count)/2:
+            new_array = np.array([pc_array[2 * i-2], pc_array[2 * i-1]], dtype=np.float32)
+    print(int(count/2))
+    # for i in range(1,count) :
+    #     new_array = np.array(pc_array)
+    print(new_array)
+    pc = pcl.PointCloud(new_array)
+    print(pc)
+    # pcl.save(pc, "11.pcd")
     # print(pc_array[0], pc_array[1])
     bag.close()
 
 if __name__ == '__main__':
     # example()
+    # eee()
     read_bagfile()
