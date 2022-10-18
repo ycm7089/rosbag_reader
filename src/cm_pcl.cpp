4c726c461300446dc3a74d31263f2783cb7389cb
ycm7089 <ycm7089@naver.com>
2022-10-18 17:51:55 +0900

    K_matrix

--- src/cm_pcl.cpp
+++ src/cm_pcl.cpp
@@ -28,10 +28,26 @@ void topiccb(const livox_ros_driver::CustomMsg::ConstPtr& msg)
         point_xyz.y = msg->points[i].y;
         point_xyz.z = msg->points[i].z;
         cloud.push_back(point_xyz);
-        ROS_INFO("%.3f %.3f %.3f", point_xyz.x, point_xyz.y, point_xyz.z);
     }
-    // cout << cloud << endl;
-    // printf(cloud);
+    // ROS_INFO("%.3f %.3f %.3f", point_xyz.x, point_xyz.y, point_xyz.z);
+
+    // cout << cloud.at(msg->point_num-1).x << " " << cloud.at(msg->point_num-1).y << " " << cloud.at(msg->point_num-1).z << endl;
+    // cloud and point_xyz match complete
+    cloud.clear();
+}
+
+void asdad()
+{
+
+}
+
+void cm_matrix()
+{
+    // realsense camera K matrix -> fx fy cx cy
+    Eigen::Matrix3d K_matrix;
+    
+    K_matrix << 601.654608, 0.000000, 326.296932, 0.000000, 603.568857, 244.054371, 0.000000, 0.000000, 1.000000; 
+    cout << K_matrix(0,1) << " " << K_matrix(1,1) << " " <<K_matrix(2,1) << endl;
 }
 
 int main(int argc, char** argv)
@@ -41,7 +57,7 @@ int main(int argc, char** argv)
 
     ros::Subscriber topic_sub;
     topic_sub = nh.subscribe("/livox/lidar", 1000, topiccb);
- 
+    cm_matrix();
     cv::Mat image;
     ros::spin();
     return 0;
