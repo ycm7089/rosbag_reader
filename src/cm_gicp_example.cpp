75f60486790c4eb2eb1a865589f748ad3e968bc5
ycm7089 <ycm7089@naver.com>
2023-06-02 10:42:30 +0900

    Complete G-ICP example

--- src/cm_gicp_example.cpp
+++ src/cm_gicp_example.cpp
@@ -32,11 +32,11 @@ int main(int argc, char** argv)
         pcl::PointCloud<pcl::PointXYZRGB>::Ptr src(new pcl::PointCloud<pcl::PointXYZRGB>);
         pcl::PointCloud<pcl::PointXYZRGB>::Ptr tgt(new pcl::PointCloud<pcl::PointXYZRGB>);
         
-        pcl::io::loadPCDFile<pcl::PointXYZRGB> ("/home/kimm/pcd_img_data/resaved_pcd/0.pcd", *src); //* load the file
+        pcl::io::loadPCDFile<pcl::PointXYZRGB> ("/home/kimm/pcd_img_data/room_scan1.pcd", *src); //* load the file
         
-
+        // 앞으로 10m 전진 시킨 tgt를 만듬
         Eigen::Matrix4f tf;
-        tf << 1, 0, 0, 2.0,
+        tf << 1, 0, 0, 10.0,
                 0, 1, 0, 0.0,
                 0, 0, 1, 0.0,
                 0, 0, 0, 1.0;
@@ -44,22 +44,33 @@ int main(int argc, char** argv)
         pcl::transformPointCloud(*src,*tgt,tf);
 
         pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> gicp;
-        gicp.setMaxCorrespondenceDistance(1.0);
+        gicp.setMaxCorrespondenceDistance(0.1);
         gicp.setTransformationEpsilon(0.001);
-        gicp.setMaximumIterations(1000);
+        gicp.setMaximumIterations(3000);
 
         pcl::PointCloud<pcl::PointXYZRGB>::Ptr align(new pcl::PointCloud<pcl::PointXYZRGB>);
+        
+        // 걸리는 시간 측정
+        chrono::system_clock::time_point t_start = chrono::system_clock::now();
 
+        // Registration 시행
         gicp.setInputSource(src);
         gicp.setInputTarget(tgt);
         gicp.align(*align);
 
+        chrono::system_clock::time_point t_end = chrono::system_clock::now();
+        /*******************************************/
+        chrono::duration<double> t_reg = t_end - t_start;
+        cout<<"Takes "<<t_reg.count()<<" sec..."<<endl;
+
+        // Ouputs
         Eigen::Matrix4f src2tgt = gicp.getFinalTransformation();
         double score = gicp.getFitnessScore();
+        bool is_converged = gicp.hasConverged(); // 수렴여부
 
         cout << src2tgt << endl;
         cout << score << endl;
-
+        cout << is_converged << endl;
 
         pcl::toROSMsg(*tgt, trans_gicp);
             
